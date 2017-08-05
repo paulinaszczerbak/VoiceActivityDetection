5c29d7e5c7b28ba3fcc602aaaa158efdf0545386
paulinaszczerbak <paulinaszczerbak@gmail.com>
2017-08-05 15:57:46 +0200

    Removed folder from repository

--- zsiciarz-aquila-d5e3bde/lib/unittestpp/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/paulina/github/VoiceActivityDetection/zsiciarz-aquila-d5e3bde/lib/unittestpp
-# Build directory: /home/paulina/github/VoiceActivityDetection/zsiciarz-aquila-d5e3bde/lib/unittestpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestUnitTest++ "/home/paulina/github/VoiceActivityDetection/zsiciarz-aquila-d5e3bde/lib/unittestpp/TestUnitTest++")
--- zsiciarz-aquila-d5e3bde/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/paulina/github/VoiceActivityDetection/zsiciarz-aquila-d5e3bde/tests
-# Build directory: /home/paulina/github/VoiceActivityDetection/zsiciarz-aquila-d5e3bde/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Aquila_TEST_SUITE "aquila_test")
